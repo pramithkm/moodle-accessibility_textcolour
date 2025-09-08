23cf41325c7bf1b49e1aae8cda5843b06a4d37db
Ponlawat W. <ponlawat_w@outlook.co.th>
2025-09-08 15:11:49 +0700

    Bumped version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'accessibility_textcolour';
-$plugin->release = '2.0.2';
+$plugin->release = '2.0.3';
 $plugin->version = 2025021800;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
